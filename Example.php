049cf4a8086b0d209fd558a166ea901d81e68caf
prajubhattarai <prajubhattarai3@gmail.com>
2025-12-06 18:48:36 +0545

    Add files via upload

---
+++ Example.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>echo or print</title>
+</head>
+<body>
+    <?php
+    echo"Example of echo statement<br/>";
+  $subject = "Scripting Language";
+  $page = 305;
+  echo "Example of echo statement<br/>";
+  $subject = "Scripting Language";
+  $page = 305;
+  echo $subject."<br/>";
+  echo "Total no of page is:"$.page;
+  //invalid
+  //print"Total no of  page into book is",$page;
+  print "<br/> Total no of page into book is",$page;
+  print"<br/> Tribhuwan University";
+  ?>
+</body>
+</html>
