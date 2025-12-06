781c0dc9c3c4a1ee5d74d12043b21b093b5521de
prajubhattarai <prajubhattarai3@gmail.com>
2025-12-06 18:55:39 +0545

    Add files via upload

---
+++ Variable .php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Varible Define</title>
+</head>
+<body>
+    <h1>Define Variable</h1>
+    <?php
+    $name;
+    $address ="Koteshwor, Kathmandu-31, Kathmandu";
+    $name="Ram Kumar Gautam"
+    echo $name;
+    echo<br/>
+    echo 'My name is' .$name;
+    echo'<br/>';
+    echo 'I am from'. $address;
+    ?>
+    </body>
+    </html>
+
+
+
