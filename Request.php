4393980f92fc66fb41d1b5e7fb324ec6ce753796
prajubhattarai <prajubhattarai3@gmail.com>
2025-12-06 19:30:10 +0545

    Add files via upload

---
+++ Request.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<tittle> $_REQUEST Example</tittle>
+</head>
+<body>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    Username:<input type="text" name="username">
+    <input type="submit">
+</form>
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+ $username=$_REQUEST['username']
+ if(empty(username)){
+    echo"Username is empty";
+ }else{
+    echo $username;
+
+ }
+ }
+ ?>
