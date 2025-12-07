f79d57e104b1806d04012022ce929d2e030a2342
prajubhattarai <prajubhattarai3@gmail.com>
2025-12-07 08:47:14 +0545

    Add files via upload

---
+++ Array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Numeric Array</title>
+</head>
+<body>
+    <?php
+    $animals = array("Buffalo","Lion","Tiger","Wolf");
+    for($i=0; $i<count($animals):$i++){
+        echo $animal[$i];
+        echo"</br>"
+    }
+    ?>
+    
+</body>
+</html>
