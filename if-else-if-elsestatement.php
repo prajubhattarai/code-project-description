b8986c10a7a0cf1fc30dd658d072a7ee119e6e23
prajubhattarai <prajubhattarai3@gmail.com>
2025-12-07 08:34:19 +0545

    Add files via upload

---
+++ if-else-if-elsestatement.php
@@ -0,0 +1,28 @@
+<?php
+$char = "a";
+if($char=="a")
+{
+    echo $char,"is vowel";
+}
+else if($char=="e")
+{
+    echo $char."is vowel";
+} 
+else if ($char=="i")
+{
+    echo $char."is vowel";
+
+}
+else if ($char=="o")
+{
+    echo $char."is vowel";
+}
+else if ($char=="u")
+{
+    echo $char."is vowel";
+}
+else
+{
+    echo $char. "is consonent";
+}
+?>
